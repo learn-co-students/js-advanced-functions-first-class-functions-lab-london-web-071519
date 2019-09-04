2191d8b202d6c70bfae1554f150e024cb0d7f6d8
Roderick Cardenas <roderickcardenas1@gmail.com>
2019-09-04 13:53:31 +0100

    FirstClass Functions

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = (array) => {
+    return array.slice(0,2)
+}
+
+const returnLastTwoDrivers = (array) => {
+    return array.slice(array.length - 2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = (number) => {
+    return function() {
+        return number * number
+    }
+}
+
+const fareDoubler = (number) => {
+    return number * 2
+}
+
+const fareTripler = (number) => {
+    return number * 3
+}
+
+const selectDifferentDrivers = (array, func) => {
+    return func(array)
+}
